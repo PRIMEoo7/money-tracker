ddd8486b6dee69aec21e4c91614b3d543d383b0e
Suraj Moolya <suraj.moolya@energage.com>
2024-04-16 08:48:13 +0530

    feat:Money tracker app(some changes are remaining)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,108 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { useEffect } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  const [datetime, setDatetime] = useState("");
+  const [description, setDescription] = useState("");
+  const [transactions, setTransactions] = useState([]);
+
+  useEffect(() => {
+    getTransactions()
+      .then((transactions) => {
+        console.log("Transactions data:", transactions); // Log the data received from the API
+        setTransactions(transactions);
+      })
+      .catch((error) => {
+        console.error("Error fetching transactions:", error); // Log any errors that occur during data fetching
+      });
+  }, []);
+
+  async function getTransactions() {
+    const url = process.env.REACT_APP_API_URL + "/transactions";
+    const response = await fetch(url);
+    return response.json();
+  }
+
+  function addNewTransaction(ev) {
+    ev.preventDefault();
+    const url = process.env.REACT_APP_API_URL + "/transaction";
+    const price = name.split(" ")[0];
+    fetch(url, {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify({
+        name: name.substring(price.length + 1),
+        price,
+        description,
+        datetime,
+      }),
+    }).then((response) => {
+      response.json().then((json) => {
+        console.log("result", json);
+        setName("");
+        setDatetime("");
+        setDescription("");
+      });
+    });
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>
+        $400<span>.00</span>
+        {transactions.length}
+      </h1>
+      <form onSubmit={addNewTransaction}>
+        <div className="basics">
+          <input
+            type="text"
+            value={name}
+            onChange={(ev) => setName(ev.target.value)}
+            placeholder={"+200 for Moto TV"}
+          />
+          <input
+            type="datetime-local"
+            value={datetime}
+            onChange={(ev) => setDatetime(ev.target.value)}
+          />
+        </div>
+        <div className="description">
+          <input
+            type="text"
+            value={description}
+            onChange={(ev) => setDescription(ev.target.value)}
+            placeholder={"description"}
+          />
+        </div>
+        <button type="submit">Add new Transaction</button>
+      </form>
+      <div className="transactions">
+        {transactions.length > 0 &&
+          transactions
+            .map((transaction) => {
+              return (
+                <div className="transaction">
+                  <div className="left">
+                    <div className="name">{transaction.name}</div>
+                    <div className="description">{transaction.description}</div>
+                  </div>
+                  <div className="right">
+                    <div
+                      className={
+                        "price-" + (transaction.price < 0 ? "red" : "green")
+                      }
+                    >
+                      {transaction.price}
+                    </div>
+                    <div className="datetime">{transaction.datetime}</div>
+                  </div>
+                </div>
+              );
+            })
+            .reverse()}
+      </div>
+    </main>
   );
 }
 
---
+++ src/api/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const Transaction = require("./models/Transaction.js");
+const cors = require("cors");
+require("dotenv").config();
+const { default: mongoose } = require("mongoose");
+
+app.use(cors());
+app.use(express.json());
+// app.get("/api/test", (req, res) => {
+//   res.json("test ok");
+// });
+
+app.post("/api/transaction", async (req, res) => {
+  const { name, price, description, datetime } = req.body;
+  //console.log(process.env.MONGO_URL);
+  await mongoose.connect(process.env.MONGO_URL);
+  const transaction = await new Transaction({
+    name,
+    price,
+    description,
+    datetime,
+  }).save();
+  //const savedTransaction = await transaction.save();
+  res.json(transaction);
+});
+
+app.get("/api/transactions", async (req, res) => {
+  await mongoose.connect(process.env.MONGO_URL);
+  const transactions = await Transaction.find();
+  res.json(transactions);
+});
+
+app.listen(4040);
